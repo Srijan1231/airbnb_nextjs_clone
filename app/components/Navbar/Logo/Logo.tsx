863f33252b76b10fc9ce77472fd610b92a58bff7
srijan <srijan.dhl21@gmail.com>
2023-10-20 12:24:40 +1100

    modal nearly complete

--- app/components/Navbar/Logo/Logo.tsx
+++ app/components/Navbar/Logo/Logo.tsx
@@ -1,12 +1,17 @@
-'use client'
+"use client";
 
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 
 export const Logo = () => {
-  const router = useRouter()
+  const router = useRouter();
   return (
-    <Image alt="Logo" className="hidden md:block cursor-pointer" height="100" width='100' src='/images/logo.png'
+    <Image
+      alt="Logo"
+      className="hidden md:block cursor-pointer"
+      height="100"
+      width="100"
+      src="/images/logo.png"
     />
-  )
-}
+  );
+};
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,31 +1,32 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
-import { Nunito } from 'next/font/google'
-import { Navbar } from './components/Navbar/Navbar';
-import { Modal } from './components/Modal/Modal';
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { Nunito } from "next/font/google";
+import { Navbar } from "./components/Navbar/Navbar";
 
-const inter = Inter({ subsets: ['latin'] })
+import { RegisterModal } from "./components/Modal/RegisterModal";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Airbnb',
-  description: 'Airbnb clone',
-}
+  title: "Airbnb",
+  description: "Airbnb clone",
+};
 const font = Nunito({
-  subsets:['latin'],
-})
+  subsets: ["latin"],
+});
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Navbar/>
-        <Modal/>
+        <Navbar />
+        <RegisterModal />
         {children}
-        </body>
+      </body>
     </html>
-  )
+  );
 }
