a068f3b040412510fd0beab411c895b70a0d168f
srijan <srijan.dhl21@gmail.com>
2023-10-30 23:28:50 +1100

    individual listing done

--- app/components/Listing/ListingHead.tsx
+++ app/components/Listing/ListingHead.tsx
@@ -3,6 +3,7 @@ import { useCountries } from "@/app/hooks/useCountries";
 import { SafeUser } from "@/app/types";
 import { Heading } from "../Heading/Heading";
 import Image from "next/image";
+import { HeartButton } from "../HeartButton";
 
 interface ListingHeadProps {
   title: string;
@@ -28,7 +29,15 @@ const ListingHead: React.FC<ListingHeadProps> = ({
         subtitle={`${location?.region},${location?.label}`}
       />
       <div className="w-full h-[60vh] overflow-hidden rounded-xl relative">
-        <Image />
+        <Image
+          alt="Image"
+          src={imageSrc}
+          fill
+          className="object-cover w-full"
+        />
+        <div className="absolute top-5 right-5">
+          <HeartButton listingId={id} currentUser={currentUser} />
+        </div>
       </div>
     </>
   );
--- app/listings/[listingId]/ListingClient.tsx
+++ app/listings/[listingId]/ListingClient.tsx
@@ -5,6 +5,7 @@ import { useMemo } from "react";
 import { categories } from "@/app/components/Navbar/Categories/Categories";
 import { Container } from "@/app/components/Container";
 import ListingHead from "@/app/components/Listing/ListingHead";
+import ListingInfo from "@/app/components/Listing/ListingInfo";
 
 interface ListingClientProps {
   reservation?: Reservation[];
@@ -29,6 +30,17 @@ export const ListingClient: React.FC<ListingClientProps> = ({
             id={listing.id}
             currentUser={currentUser}
           />
+          <div className="grid grid-cols-1 md:grid-cols-7 md:gap-10 mt-6">
+            <ListingInfo
+              user={listing.user}
+              category={category}
+              description={listing.description}
+              roomCount={listing.roomCount}
+              guestCount={listing.guestCount}
+              bathroomCount={listing.bathroomCount}
+              locationValue={listing.locationValue}
+            />
+          </div>
         </div>
       </div>
     </Container>
