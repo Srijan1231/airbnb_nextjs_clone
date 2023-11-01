5e1840463f2e8c913caf8b8d758931fc89a6ddcd
srijan <srijan.dhl21@gmail.com>
2023-11-01 23:36:09 +1100

    calender done

--- app/components/Inputs/Calendar.tsx
+++ app/components/Inputs/Calendar.tsx
@@ -1,7 +1,8 @@
 "use client";
 
 import { DateRange, Range, RangeKeyDict } from "react-date-range";
-
+import "react-date-range/dist/styles.css";
+import "react-date-range/dist/theme/default.css";
 interface CalenderProps {
   value: Range;
   onChange: (value: RangeKeyDict) => void;
@@ -12,7 +13,18 @@ const Calendar: React.FC<CalenderProps> = ({
   onChange,
   disabledDates,
 }) => {
-  return <DateRange></DateRange>;
+  return (
+    <DateRange
+      rangeColors={["#262626"]}
+      ranges={[value]}
+      date={new Date()}
+      onChange={onChange}
+      direction="vertical"
+      showDateDisplay={false}
+      minDate={new Date()}
+      disabledDates={disabledDates}
+    />
+  );
 };
 
 export default Calendar;
